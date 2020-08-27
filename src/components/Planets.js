f9c844f0c23344e490d1da25c56b8326ca3f6bbc
blestab <blestab@users.noreply.github.com>
2020-08-28 00:51:42 +0200

    Pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useQuery } from 'react-query'
+import { usePaginatedQuery } from 'react-query'
 import Planet from './Planet'
 
 const fetchPlanets = async ( key, page ) => {
@@ -9,25 +9,36 @@ const fetchPlanets = async ( key, page ) => {
 
 const Planets = () => {
     const [ page, setPage] = useState(1)
-    const { data , status } = useQuery([ 'planets' , page ], fetchPlanets, {
+    const {
+        resolvedData,
+        latestData ,
+        status
+    } = usePaginatedQuery([ 'planets' , page ], fetchPlanets, {
         staleTime: 5000,
         retry: 3,
     });
-    console.log(data);
+    console.log(latestData);
 return (
     <div>
         <h2>Planets</h2>
-
-        <button onClick={ () => setPage(1)}>Page 1</button>
-        <button onClick={ () => setPage(2)}>Page 2</button>
-        <button onClick={ () => setPage(3)}>Page 3</button>
 
         {status === 'loading' && (<div>Loading ...</div>)}
 
         {status === 'error' && (<div>The was an error fetching data!</div>)}
 
         {status === 'success' && (
-            data.results.map(planet => <Planet key={planet.name} planet={planet}/>)
+            <>
+                <button
+                    onClick={ () => setPage(old => Math.max(old - 1, 1)) }
+                    disabled={page === 1}
+                >Previous Page</button>
+                <span>{ page }</span>
+                <button
+                    onClick={ () => setPage( old => (!latestData || !latestData.next ? old : old + 1) )}
+                    disabled={ (!latestData || !latestData.next) }
+                >Next Page</button>
+                <div>{ resolvedData.results.map( planet => <Planet key={planet.name} planet={ planet }/> ) }</div>
+            </>
         )}
     </div>
 )
